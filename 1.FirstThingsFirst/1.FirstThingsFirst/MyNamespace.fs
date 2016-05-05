b9d10e04c985490c1dc0d23b7bb84ef40f9a2e37
merdigon <szymon.nowak3848@gmail.com>
2016-05-05 14:32:03 +0200

    New changes in lesson 1

---
+++ 1.FirstThingsFirst/1.FirstThingsFirst/MyNamespace.fs
@@ -0,0 +1,16 @@
+﻿namespace MyNamespace
+
+type Color =
+    | Red
+    | Green
+    | Blue
+    | Yellow
+
+type Thing =
+    | Ball
+    | Car
+    | House
+
+namespace MyNameSpace.Organized
+
+type Orginize = { color : MyNamespace.Color; thing : MyNamespace.Thing }
